2c976a8429aa14c2707e780faffbd80f51a17d45
Fidel Leal <127764786+fidel-techprep@users.noreply.github.com>
2023-09-28 17:26:32 +0000

    add form to update user

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:path_username", { :controller => "users", :action => "show" })
   post("/add_user", { :controller => "users", :action => "create" })
+  post("/update_user/:modify_id", { :controller => "users", :action => "update" })
 
   get("/photos", { :controller => "photos", :action => "index" })
   get("/photos/:path_id", { :controller => "photos", :action => "show" })
